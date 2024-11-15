448e01584a75c4a209697d47504277f72151b3d8
Donalí <00084417@uca.edu.sv>
2024-11-15 11:37:03 -0600

    first commit

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 80, // Longitud máxima de línea (Google usa 80)
+  tabWidth: 2, // Google usa 2 espacios para indentación
+  useTabs: false, // No usa tabs, sino espacios
+  semi: true, // Siempre usar punto y coma
+  singleQuote: true,
+  trailingComma: "es5", // Comas al final en objetos y arrays (ES5)
+  bracketSpacing: false, // Sin espacio entre llaves {key: value}
+  arrowParens: "always", // Siempre usar paréntesis en funciones flecha (Google lo requiere)
+  endOfLine: "lf", // Usar LF como final de línea (evita errores en diferentes sistemas operativos)
+};
