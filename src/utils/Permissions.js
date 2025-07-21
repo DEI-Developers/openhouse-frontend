57f97cd26c10e9ee06ccd7d426cc60ae61dc9c15
Marroquin02 <carlosmarroquin600000@gmail.com>
2025-07-21 11:17:09 -0600

    feat(Permissions): add new participant-related permissions
    - Add permissions for participant CRUD operations and viewing participants by faculty/career/all

--- src/utils/Permissions.js
+++ src/utils/Permissions.js
@@ -3,4 +3,10 @@ export default {
   MANAGE_ROLES: 'manage_roles',
   MANAGE_EVENTS: 'manage_events',
   MANAGE_PARTICIPANTS: 'manage_participants',
+  DELETE_PARTICIPANTS: 'delete_participants',
+  CREATE_PARTICIPANT: 'create_participant',
+  UPDATE_PARTICIPANT: 'update_participant',
+  VIEW_FACULTY_PARTICIPANTS: 'view_faculty_participants',
+  VIEW_ALL_PARTICIPANTS: 'view_all_participants',
+  VIEW_CAREER_PARTICIPANTS: 'view_career_participants',
 };
